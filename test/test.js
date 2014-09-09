54100285cbe587391153c6d55a887d4992202c5f
git <stefan.thies@me.com>
2014-09-10 00:48:34 +0200

    initial repository

--- test/test.js
+++ test/test.js
@@ -6,7 +6,7 @@ describe("spm custom metrics ", function(){
 
     it("should pass", function(done){
         var spmcm = require('../lib/index.js')
-        var token = '34569637-0684-4f67-9029-a91e4ab4efb6'
+        var token = process.env.TOKEN
         var spmcm = new spmcm (token, 0)
         spmcm.collectProcessMetrics()
         spmcm.send()
