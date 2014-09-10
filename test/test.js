3c3b9b2ef93ff9b0f99eeb473729ec31dfac8d7f
git <stefan.thies@me.com>
2014-09-10 20:07:52 +0200

    added event api

--- test/test.js
+++ test/test.js
@@ -10,4 +10,17 @@ describe("spm custom metrics ", function () {
         done();
     });
 
+    it("should pass", function (done) {
+        var spmcm = require('../lib/index.js')
+        var token = process.env.TOKEN
+        console.log(token)
+        var spmcm = new spmcm(token, 0)
+        spmcm.logEvent('test1', 'High', 'this is an event message', 'testing', ['test', 'node-spm'], 'Stefan Thies', 'this is any data as string or base64 encoded', function (err, res) {
+            console.log(res.body)
+            done();
+        })
+
+    });
+
+
 });
